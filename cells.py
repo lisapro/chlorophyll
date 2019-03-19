11011b8147dc9c6475ab7183b74b23edc27e8d8b
lisapro <elisaveta.pro@gmail.com>
2019-03-19 11:19:26 +0100

    new plots

--- cells.py
+++ cells.py
@@ -10,6 +10,7 @@ import seaborn as sns #sets up styles and gives us more plotting options
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 sns.set()
+sns.set_context("paper")
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 import matplotlib.ticker as mtick 
@@ -45,7 +46,8 @@ def plot_station(path_cell,path_carb_bugoynes,path_chla,sheet,stationname,to_tit
     df3 = df3.replace({ r'< 0.17$'}, {0}, regex=True)         
     df3 = df3.replace({ r'< 0.16$'}, {0}, regex=True)     
     df3 = df3.replace({ r'< 0,16$'}, {0}, regex=True)     
-    df3 = df3.replace({ r'< 0,22$'}, {0}, regex=True)    
+    df3 = df3.replace({ r'< 0,22$'}, {0}, regex=True)   
+    df3 = df3.replace({ r'< 0.21$'}, {0}, regex=True)     
     df3 = df3.replace({ r'<*$'}, {0}, regex=True) 
     
     def get_sum(df):
@@ -119,8 +121,8 @@ def plot_station(path_cell,path_carb_bugoynes,path_chla,sheet,stationname,to_tit
 
     fig.tight_layout(pad = 0.3)
 
-    plt.savefig(r'{}\Plot\{}.png'.format(path,to_title))
-    #plt.show()
+    #plt.savefig(r'{}\Plot\{}.png'.format(path,to_title))
+    plt.show()
     #print (new_df)
 
 path_chla = '\klorofyll Norskehavet 1_3_Barentshavet.xlsx'
@@ -155,14 +157,37 @@ path_carb_Setså = r'\Norskehavet Nord 2\setså_karbon_2017_2018.xlsx'
 path_cell_Reisafjorden = r'\Norskehavet Nord 3\Reisafj_celler.L_2018.xlsx'
 path_carb_Reisafjorden = r'\Norskehavet Nord 3\Reisafj_karbon_2018.xlsx'
 
+path_cell_Spilderbu =  r'\Norskehavet Nord 3\Spilderbu_celler.L_2018.xlsx'
+path_carb_Spilderbu =  r'\Norskehavet Nord 3\spilderbu_karbon_2018.xlsx'
+
+path_cell_Storbukta =  r'\Norskehavet Nord 3\Storbukta_celler.L_2018.xlsx'
+path_carb_Storbukta =  r'\Norskehavet Nord 3\Storbukta_karbon_2018.xlsx'
+
+path_cell_Sørfj =  r'\Norskehavet Nord 3\Sørfj_ytre_celler.L_2018.xlsx'
+path_carb_Sørfj =  r'\Norskehavet Nord 3\Sørfj_ytre_karbon_2018.xlsx'
+
+path_cell_Ullsfj =  r'\Norskehavet Nord 3\Ullsfj_celler.L_2918.xlsx'
+path_carb_Ullsfj =  r'\Norskehavet Nord 3\Ullsfj_karbon_2018.xlsx'
+
+path_cell_Ytre =  r'\Norskehavet Nord 3\Ytre kvæn_celler.L_2018.xlsx'
+path_carb_Ytre =  r'\Norskehavet Nord 3\Ytre kvæn_karbon_2018.xlsx'
+
+
+
 '''plot_station(path_cell_bugoynes,     path_carb_bugoynes,    path_chla, sheet = 'Barentshavet', stationname ='Bugøynes',     to_title = 'VT22')    
 plot_station(path_cell_langfjordnes, path_carb_langfjordnes,path_chla, sheet = 'Barentshavet', stationname ='langfjordnes', to_title = 'langfjordnes')    
 plot_station(path_cell_Tanafjord,    path_carb_Tanafjord,   path_chla, sheet = 'Barentshavet', stationname ='Tanafjorden',  to_title = 'VR24')
 
 plot_station(path_cell_kongsbakk,    path_carb_kongsbakk,   path_chla, sheet = 'Nord1Norskehavet', stationname ='Kongsbakkneset',      to_title = 'Kongsbakkneset')
 plot_station(path_cell_straumsfj,    path_carb_straumsfj,   path_chla, sheet = 'Nord1Norskehavet', stationname ='StraumsfjordenVR54',  to_title = 'VR54')
-plot_station(path_cell_tjukkenes,    path_carb_tjukkenes,   path_chla, sheet = 'Nord1Norskehavet', stationname ='Tjukkeneset',         to_title = 'Tjukkeneset')'''
+plot_station(path_cell_tjukkenes,    path_carb_tjukkenes,   path_chla, sheet = 'Nord1Norskehavet', stationname ='Tjukkeneset',         to_title = 'Tjukkeneset')
 
 plot_station(path_cell_alvenes,      path_carb_alvenes,     path_chla, sheet = 'Nord2Norskehavet', stationname ='Alvenes',  to_title = 'Alvenes')
 plot_station(path_cell_Setså,        path_carb_Setså,       path_chla, sheet = 'Nord2Norskehavet', stationname ='Setså',  to_title = 'Setså')
 plot_station(path_cell_Reisafjorden,        path_carb_Reisafjorden,       path_chla, sheet = 'Norskehavet3', stationname ='Reisafjorden',  to_title = 'Reisafjorden')
+
+plot_station(path_cell_Spilderbu,        path_carb_Spilderbu,       path_chla, sheet = 'Norskehavet3', stationname ='Spilderbukta',  to_title = 'Spilderbukta')
+plot_station(path_cell_Storbukta,        path_carb_Storbukta,       path_chla, sheet = 'Norskehavet3', stationname ='Storbukta',  to_title = 'Storbukta')
+plot_station(path_cell_Sørfj,        path_carb_Sørfj,       path_chla, sheet = 'Norskehavet3', stationname ='SørfjordenYtre',  to_title = 'SørfjordenYtre')
+plot_station(path_cell_Ullsfj,        path_carb_Ullsfj,       path_chla, sheet = 'Norskehavet3', stationname ='Ullsfjorden',  to_title = 'Ullsfjorden')'''
+plot_station(path_cell_Ytre,        path_carb_Ytre,       path_chla, sheet = 'Norskehavet3', stationname ='YtreKvænangen',  to_title = 'YtreKvænangen')
